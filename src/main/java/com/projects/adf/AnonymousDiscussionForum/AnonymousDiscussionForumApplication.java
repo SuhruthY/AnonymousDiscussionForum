632f7e5d593d248c81f16b54f89adb9ec8b73789
Suhruth Yambakam <71488673+SuhruthY@users.noreply.github.com>
2024-11-02 02:30:08 +0530

    Add files via upload

---
+++ src/main/java/com/projects/adf/AnonymousDiscussionForum/AnonymousDiscussionForumApplication.java
@@ -0,0 +1,13 @@
+package com.projects.adf.AnonymousDiscussionForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousDiscussionForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousDiscussionForumApplication.class, args);
+	}
+
+}
